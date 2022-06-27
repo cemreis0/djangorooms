64f5737acd2983be063d48d8a4588269bf293a67
cemreis0 <cemmreis@gmail.com>
2022-06-27 14:25:20 +0300

    added room and message models

--- djangorooms/main/models.py
+++ djangorooms/main/models.py
@@ -1,38 +1,25 @@
 from django.db import models
 
 
-class User(models.Model):
-    name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(unique=True, null=True)
-    bio = models.TextField(null=True)
-
-
-class Topic(models.Model):
-    name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-
-
 class Room(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    #host = 
+    #topic = 
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+    #participants = 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.name
 
 
 class Message(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #user = 
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
-        return self.body[0:50]
+        return self.body
--- djangorooms/main/views.py
+++ djangorooms/main/views.py
@@ -1,7 +1,16 @@
+from math import frexp
 from django.shortcuts import render
+from .models import Room, Message
+
 
 def home(request):
-    return render(request, 'main/home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'main/home.html', context)
 
-def room(request):
-    return render(request, 'main/room.html')
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    messages = Message.objects.filter(room=pk)
+    context = {'room': room, 'messages': messages}
+    return render(request, 'main/room.html', context)
