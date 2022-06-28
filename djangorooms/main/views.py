c0e4931cb0f3325c0517fd4737a0bdd3c511b4bd
cemreis0 <cemmreis@gmail.com>
2022-06-28 21:04:44 +0300

    added CRUD

--- djangorooms/main/views.py
+++ djangorooms/main/views.py
@@ -1,6 +1,7 @@
 from math import frexp
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Room, Message
+from .forms import RoomForm
 
 
 def home(request):
@@ -17,5 +18,31 @@ def room(request, pk):
 
 
 def createRoom(request):
-    context = {}
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
     return render(request, 'main/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'main/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'main/delete.html', {'obj': room})
